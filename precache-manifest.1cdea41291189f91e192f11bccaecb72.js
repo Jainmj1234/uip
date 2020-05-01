b48af2adbb8eb7d0f98735bbdf293637383a5a39
jainmj1234 <jainmj1234@gmail.com>
2020-05-01 13:19:19 +0530

    Updates

---
+++ precache-manifest.1cdea41291189f91e192f11bccaecb72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c0e795eca5c112e381ad199dea0860",
+    "url": "/uip/index.html"
+  },
+  {
+    "revision": "8ed2eeb8a0ed89a0392b",
+    "url": "/uip/static/css/main.028a8981.chunk.css"
+  },
+  {
+    "revision": "1cc70b8d8911900e0981",
+    "url": "/uip/static/js/2.3f5506b7.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/uip/static/js/2.3f5506b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed2eeb8a0ed89a0392b",
+    "url": "/uip/static/js/main.7dd12743.chunk.js"
+  },
+  {
+    "revision": "e72d1e780aafaf7351a1",
+    "url": "/uip/static/js/runtime-main.838e3c44.js"
+  }
+]);
